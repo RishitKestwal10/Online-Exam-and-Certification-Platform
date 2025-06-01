1af6130e77d546542a968ed1ece9172aaa4f6922
Rishit Kestwal <160019593+RishitKestwal10@users.noreply.github.com>
2025-06-01 11:20:08 +0000

    Dashboard

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+import TodoList from './components/TodoList';
+const TeacherDashboard = () => {
+  return (
+    <div className="p-6 space-y-6">
+      <h1 className="text-3xl font-bold text-gray-700">Welcome to Teacher Dashboard</h1>
+      <TodoList />
+    </div>
+  );
+};
+export default Dashboard;
