96ab6c4751a7829217c58bae2544adce63b06933
RishitKestwal10 <rishitkestwal@gmail.com>
2025-08-03 10:21:33 +0530

    Home Page route edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage"; // <-- Import HomePage
 import LoginRegister from "./components/auth/LoginRegister";
 import StudentDashboard from "./components/student/StudentDashboard";
 import TeacherDashboard from "./components/teacher/TeacherDashboard";
@@ -7,11 +8,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginRegister />} />
-        <Route path="/student-dashboard/*" element={<StudentDashboard />} />
-        <Route path="/teacher-dashboard/*" element={<TeacherDashboard />} />
+        <Route path="/" element={<HomePage />} /> {/* HomePage as default */}
         <Route path="/loginregister" element={<LoginRegister />} />
         <Route path="/login" element={<LoginRegister />} />
+        <Route path="/student-dashboard/*" element={<StudentDashboard />} />
+        <Route path="/teacher-dashboard/*" element={<TeacherDashboard />} />
       </Routes>
     </Router>
   );
